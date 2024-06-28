fc9ffec563e41e58ba6818a1026a2c8ec5b6612e
Edmund Ng <edmundkh.ng@mail.utoronto.ca>
2024-06-28 10:53:29 -0400

    Find name, year start & end for filtered simulations

--- missing_plev.sh
+++ missing_plev.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Function: List simulation with missing plev3d
+# Function: Find simulation with missing plev3d and build slurm command
 
-sim_with_plev=$(find $PWD/*/*/*nc | grep -E "plev" | cut -d '/' -f 7 | sort -u)
+# Simulation without plev
 
-for item in $sim_with_plev;do
-echo $item
+sim_without_plev=$(find $PWD/*/monthly*  -type d '!' -exec sh -c 'ls -1 "{}"|egrep -i -q "*plev*"' ';' -print | cut -d '/' -f 7-8 | sort -u)
+
+for item in ${sim_without_plev};do
+sim_name=$(echo ${item} | cut -d '/' -f 1)
+start_year=$(echo ${item}  | cut -d '/' -f 2 | cut -d '_' -f 3)
+end_year=$( echo ${item} | cut -d '/' -f 2 | cut -d '_' -f 4) 
+echo ${sim_name},${start_year},${end_year}
 printf '\n'
+
 done
+
 
 
 
