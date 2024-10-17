4be6e4e2d03f8125a359f9a80031ea4f9a9f40df
Edmund Ng <edmundkh.ng@mail.utoronto.ca>
2024-10-17 14:54:55 -0400

    modify file name function

--- scripts/regrid.py
+++ scripts/regrid.py
@@ -8,7 +8,6 @@ import xarray as xr
 import numpy as np
 from netCDF4 import Dataset
 import pandas as pd
-import cartopy.crs as crs
 # internal export
 from Climate_normals import get_nth_word_custom_delimiter, build_parameter
 from wrf import (getvar, ALL_TIMES, to_np,get_cartopy, latlon_coords,Constants)
@@ -96,16 +95,11 @@ def build_dataset(dirs):
     # For OBS datasets:
     else:
         # for ERA5L, CERES monthly or multiple nc
-        if '\*.nc' in dirs:
+        if dirs[-4::].lower() == '*.nc':
             ds = xr.open_mfdataset(dirs).sortby('time')
         # for ERA5slev, Rutgers
         else:
             ds = xr.open_dataset(dirs)
-    # add attrs for regridding
-    grid, name = grid_parameter(dirs)
-    ds.attrs['input_grid'] = grid
-    ds.attrs['ds_type'] = name
-    ds.attrs['ds_path'] = dirs
     return ds
 
 # Fix lat/lon before regridding
@@ -163,12 +157,13 @@ def regrid_ds(ds_in, grid_in , ds_out, grid_out, re_method = 'patch'):
         print('building weight matrix...')
         regridder = xe.Regridder(ds_in, ds_out, method = re_method)
         # build xr dataset for export, adapted from xesmf/frontend.py (Line 759 - 768). DOI: https://doi.org/10.5281/zenodo.4294774
-        w = regridder.weights.data
+        weights = regridder.weights.data
         dim = 'n_s'
         wm_ds = xr.Dataset(
-            {'S': (dim, w.data), 'col': (dim, w.coords[1, :] + 1), 'row': (dim, w.coords[0, :] + 1)}
+            {'S': (dim, weights.data), 'col': (dim, weights.coords[1, :] + 1), 'row': (dim, weights.coords[0, :] + 1)}
         )
         # writing weight matrix to disk
+        print('writing weight matrix to disk...')
         wm_ds.to_netcdf(path = wm)
         print('done')
     # Perform regridding 
@@ -187,83 +182,81 @@ def find_tem_freq(ds_in):
     Output:
     !. temporal frequency as string
     """
-    freq = xr.infer_freq(ds_in.time)
-    # if xr.infer_freq does not work
-    if freq is None:
-        delta = pd.to_timedelta(np.diff(ds_in.time)).mean()
-        if delta.days <=31 and delta.days>=28:
-            freq = 'MS'
-        elif delta.days == 7:
-            freq = 'W'
+    # check for norm 
+    if 'time' not in ds_in.coords:
+        if 'month' in ds_in.coords:
+            ds_freq = 'mon-norm'
+        elif 'season' in ds_in.coords:
+            ds_freq = 'sea-norm'
     else:
-        pass
-    # return freq name based on results
-    if 'MS' in freq or 'ME' in freq:
-        ds_freq = 'monthly'
-    elif 'W' in freq:
-        ds_freq = 'weekly'
-    elif 'Q' in freq:
-        ds_freq = 'seasonal'
-    elif 'Y' in freq:
-        ds_freq = 'yearly'
+        freq = xr.infer_freq(ds_in.time)
+        # if xr.infer_freq does not work
+        if freq is None:
+            delta = pd.to_timedelta(np.diff(ds_in.time)).mean()
+            if delta.days <=31 and delta.days>=28:
+                freq = 'MS'
+            elif delta.days == 7:
+                freq = 'W'
+        else:
+            pass
+        # return freq name based on results
+        if 'MS' in freq or 'ME' in freq:
+            ds_freq = 'monthly'
+        elif 'W' in freq:
+            ds_freq = 'weekly'
+        elif 'Q' in freq:
+            ds_freq = 'seasonal'
+        elif 'Y' in freq:
+            ds_freq = 'yearly'
     return ds_freq
 
 # Function to build filename of exported netcdf
-def build_regridded_nc_filename(ds_in):
+def build_regridded_nc_filename(ds_in, ds_type):
     """
     Function: build filename for exported regridded nc
     Inputs:
     1. ds_in: xarray dataset/dataarray of output dataset with ds_name, input_grid, output_grid, ds_path as attrs
+    2. ds_type: name of exported netcdf
     Output:
     Filename
     """
+    # get temporal frequency
+    freq = find_tem_freq(ds_in)
+    # find period
+    # for norm
+    if 'norm' in freq:
+        start_year = ds_in.attrs['norm_begin_date'][0:4]
+        end_year = ds_in.attrs['norm_end_date'][0:4]
+    # for time series
+    else:
+        start_year = str(ds_in.time[0].dt.year.values)
+        end_year = str(ds_in.time[-1].dt.year.values)
     # for WRF
-    if re.search('wrf', ds_in.attrs['ds_path'], re.I):
-        # for WRF norm
-        if re.search('-norm-', ds_in.attrs['ds_path']):
-            filename = ds_in.attrs['ds_path'].replace(ds_in.attrs['input_grid'],ds_in.attrs['output_grid'])
-        # for wrf monthly series
-        else:
+    if ds_type == 'wrf':
             # build path 
             path, scen = build_parameter(ds_in)
             # replace grid in path
-            path_new = path.replace(ds_in.attrs['input_grid'],ds_in.attrs['output_grid'])
-            # find period
-            start_year = str(ds_in.time[0].dt.year.values)
-            end_year = str(ds_in.time[-1].dt.year.values)
+            path_new = path.replace('na24',ds_in.attrs['output_grid'])
+
             # get wrf subcategory
             wrf_cat = get_nth_word_custom_delimiter(ds_in.attrs['description'], ' ', 1)
             # name for netcdf
-            filename_sub = f"{wrf_cat}_{scen}_monthly-{start_year}-{end_year}.nc"
+            filename_sub = f"{wrf_cat}_{scen}_{freq}-{start_year}-{end_year}.nc"
             # build filename
             filename = os.path.join(path_new, filename_sub)
     # for obs dataset 
-    elif re.search('Data', ds_in.attrs['ds_path']):
-        # for dataset built from multiple files
-        if ds_in.attrs['ds_path'][-4::] == '*.nc':
-            # find period
-            start_year = str(ds_in.time[0].dt.year.values)
-            end_year = str(ds_in.time[-1].dt.year.values)
-            # frequency
-            freq = find_tem_freq(ds_in)
-            # build filename
-            filename = f"{ds_in.attrs['ds_name']}_{ds_in.attrs['output_grid']}_{freq}-{start_year}-{end_year}.nc"
-        # for single file
-        else:
-            filename = ds_in.attrs['ds_path'].replace(ds_in.attrs['input_grid'],ds_in.attrs['output_grid'])
+    else:
+        filename = f"{ds_type}_{freq}-{start_year}-{end_year}.nc"
     return filename
 
 # Function for automated regridding
-def main(dir_in, dir_out, method ='patch', dir_wm = None, dir_wmn = None, to_wrf = False, export = False, dir_rgnc = None):
+def main(dir_in, dir_out, method ='patch', export = False, dir_rgnc = None):
     """
     Function: automated regridding based on file paths and assign output directories if necessary
     Inputs:
     1. dir_in: input dataset (dataset that undergoes re-gridding)
     2. dir_out: output dataset (dataset that provides reference grid)
-    3. m: regridding method* as string, patch as default
-    4. dir_wm: directory of weight matrix if not stored in current working directory
-    5. dir_wmn: directory of created weight matrix, current working directory by default
-    6. to_wrf: boolean, convert lat/lon values to wrf coordinate system, false by default
+    3. method: regridding method* as string, patch as default
     7. export: boolean, write regridded netcdf to disk, False by default
     8. dir_rgnc: directory of created regridded netcdf, current working directory by default
     * Methods avaiable: https://xesmf.readthedocs.io/en/stable/user_api.html
@@ -276,7 +269,7 @@ def main(dir_in, dir_out, method ='patch', dir_wm = None, dir_wmn = None, to_wrf
     # For WRF datasets
     if 'wrf' in dir_in or 'wrf' in dir_out:
         # build wrf grid, lat and lon
-        wrf_proj, wrf_ll = build_wrf_grid(geo_file)
+        wrf_ll = build_wrf_grid(geo_file)
         # apply to wrf dataset
         if 'wrf' in dir_in:
             ds_in = fix_latlon(ds_in, wrf_ll)
@@ -288,11 +281,6 @@ def main(dir_in, dir_out, method ='patch', dir_wm = None, dir_wmn = None, to_wrf
         ds_in = fix_latlon(ds_in)
         ds_out = fix_latlon(ds_out)
     # Perform regridding
-    if dir_wm is not None:
-        ds_in_re = regrid_ds(ds_in, ds_out, method, dir_wm = dir_wm)
-    elif dir_wmn is not None:
-        ds_in_re = regrid_ds(ds_in, ds_out, method, dir_wm_new = dir_wm_new)
-    else:
         ds_in_re = regrid_ds(ds_in, ds_out, method)
     # Convert to WRF24 coordinate system
     if to_wrf is True:
@@ -335,9 +323,6 @@ if __name__ == "__main__":
     parser.add_argument('dirs', nargs = 2, help = 'input dataset (dataset that undergoes re-gridding) output dataset (dataset that provides reference grid)')
     # Optional arguements
     parser.add_argument('-m', '--method', default = 'patch', help = 'Method for regridding')
-    parser.add_argument('-dw', '--dir_wm', default = '', help = 'Directory of weight matrix.nc exists but not in current directory')
-    parser.add_argument('-dwn', '--dir_wmn', default ='', help = 'Directory for exporting weight matrix.nc if not current directory')
-    parser.add_argument('-tw', '--to_wrf', action = 'store_true', help ='convert to WRF coordinate system')
     parser.add_argument('-ex', '--export', action = 'store_true', help = 'write regridded netcdf to disk')
     parser.add_argument('-dnc', '--dir_rgnc', default ='', help = 'Directory for exporting regridded ds.nc if not current directory')
     # convert arguments to objects
@@ -346,5 +331,4 @@ if __name__ == "__main__":
     os.system('clear')
     # run main program
     main(dir_in = args.dirs[0], dir_out = args.dirs[1], method = args.method,
-         dir_wm = args.dir_wm, dir_wmn = args.dir_wmn, to_wrf = args.to_wrf, 
          export = args.export, dir_rgnc = args.dir_rgnc)
